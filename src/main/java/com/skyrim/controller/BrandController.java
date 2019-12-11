1355b1d0c1222570eb5ad667ed2c6d2be7204d98
LastSongz <49801242+LastSongz@users.noreply.github.com>
2019-12-11 19:49:43 +0800

    Update BrandController.java
    git test

--- src/main/java/com/skyrim/controller/BrandController.java
+++ src/main/java/com/skyrim/controller/BrandController.java
@@ -23,4 +23,9 @@ public class BrandController {
     public List<TbBrand> findRedis(){
         return brandService.getRedis("skyrim");
     }
+    
+    @RequestMapping("/gitTest")
+    public List<TbBrand> GitTest(){
+        return brandService.getRedis("skyrim");
+    }
 }
